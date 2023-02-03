c5656ccb74a4ead24a4c4cb254bc4de836d4eb11
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-02 23:49:51 -0500

    add current time

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+//------------Pseudo Code provided by TA in class-------------
+// a whole day (9am - 5pm) = 9 rows/3 col in each (look at bootstrap)
+            // first column: time
+            // second : description = text area
+            // 3rd: button -> saves to local storage 
+    // create HTML for time blocks
+    // event listener for each
+
+// get current time
+
+// 3rd button to save to storage
+    // event listener (for the buttons on each row)
+    // saves text to local storage 
+    // load any saved data from local storage (getItem)
+            // look at html text area for the description 
+    
+
+    
+// current date is coming from Day.js library --- will need the CDN
+    // paste just above the js link at the bottom of html
+    // read the documentation to figure out how to manipulate it
+        // can be used to update the hours
+
+// background color needs to change based on past/present/future
+    // if currentHour > timeBlock (set class to past = grey background)
+            // else if currentHour < timeBlock (set class to future = green) background
+                // else (set class it as present = red background)
+//----------------------End of Pseudo Code---------------------------
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +51,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+//});
+
+//This variable declares the current date from the dayjs library with adjusted format
+var dayMonth = dayjs().format('dddd, MMMM DD');
+//This will dispalay the current date on the webpage
+$('#currentDay').text(dayMonth);
